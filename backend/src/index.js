8e258cdfe5ae872c9b64031d2bffaaaa66544bed
Dat Hoang <dth@Dats-MacBook-Air.local>
2025-06-05 12:11:26 -0700

    fix: update 8080 port for deploying docker image to google cloud

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,34 +4,12 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-//const PORT = 3000
-//const PORT = process.env.PORT
-//app.listen(PORT)
-//console.info(`express server running on http://localhost:${PORT}`)
-
 try {
   await initDatabase()
-  const PORT = process.env.PORT
-  app.listen(PORT)
-  console.info(`express server running on http://localhost:${PORT}`)
+  const PORT = process.env.PORT || 8080
+  app.listen(PORT, '0.0.0.0', () => {
+    console.info(`express server running on http://localhost:${PORT}`)
+  })
 } catch (err) {
   console.error('error connecting to database:', err)
 }
-
-/**
- * npm run start
- * 
- * successfully connected to database: mongodb://localhost:27017/blog
- * express server running on http://localhost:3000
- * 
- * running datase using docker: 
-docker run -i -t ubuntu:24.04 /bin/bash
-uname -a
-exit
--
-docker ps
-docker run -d --name dbserver -p 27017:27017 --restart unless-stopped monggo
-
-mongosh mongodb://localhost:27017/ch2
-
- */
